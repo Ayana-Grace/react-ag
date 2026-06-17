806929c9f9ef14a3a47ca6ab528dceabfbcf63b6
Ayana-Grace <chocochipcookie257@gmail.com>
2026-06-17 13:34:04 +0530

    work on react

--- proj1/src/App.jsx
+++ proj1/src/App.jsx
@@ -1,26 +1,24 @@
-import React from 'react'
-import {Routes ,Route ,Link ,BrowserRouter} from 'react-router-dom'
+
+import { Routes, Route, Link, BrowserRouter } from 'react-router-dom'
 import HomePage from './components/HomePage'
 import AboutPage from './components/AboutPage'
 import ContactPage from './components/ContactPage'
 
 const App = () => {
   return (
-    <>
     <BrowserRouter>
-     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/contact">Contact</Link>
-    </nav>
-    <Routes>
-      <Route path="/" element={<HomePage></HomePage>}/>
-      <Route path="/about" element={<AboutPage></AboutPage>}/>
-      <Route path="/contact" element={<ContactPage></ContactPage>}/>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
 
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
-      </BrowserRouter>
-    </>
+    </BrowserRouter>
   )
 }
 
--- proj1/src/components/Arraytask.jsx
+++ proj1/src/components/Arraytask.jsx
@@ -5,25 +5,18 @@ function Arraytask()
 {
         const[count,setCount]=useState(0);
         const handleClick=()=>{
-            if(count<reactionArray.length-1){
+            if(count<reactionArray.length-1)
                 setCount(count+1)
-            }
-            else{
-                setCount(count+1)
-            }
+            
         
         };
     
   return (
    <div className="container">
-    if({count}>11)
-    {
+    
     <h2>Likes:{count}{reactionArray[count]}</h2>
-}
-else
-{
-    <h2>Likes:{count}{reactionArray[12]}</h2>
-}
+
+
     <FcLike size={40} onClick={handleClick} style={{cursor:"pointer"}}/>
     </div>
   );
